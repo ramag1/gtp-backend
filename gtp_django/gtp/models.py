ef038b10c7c7f7068a81e1329a2c3dfc5cde74b9
rmagley <rachelmagley@gmail.com>
2022-04-18 20:03:36 -0700

    Complete admin test

--- gtp_django/gtp/models.py
+++ gtp_django/gtp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Inquiry(models.Model):
+    email = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    comments = models.TextField()
+
+def __str__(self):
+        return self.email
